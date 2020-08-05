94612337f2830a8af3ff76937091f3ee71a7f8aa
AwesomeyouthZhang <awesomeyouthzhang@AwesomeyouthZhangdeiMac.local>
2020-08-05 18:32:43 +0800

    🎉complete proj1fold

--- proj1gold/AssertEqualsStringDemo.java
+++ proj1gold/AssertEqualsStringDemo.java
@@ -6,7 +6,7 @@ public class AssertEqualsStringDemo {
     @Test
     public void test1() {
         int expected = 5;
-        int actual = StdRandom.uniform(9);
+        int actual =  StdRandom.uniform(9);
         assertEquals("Oh noooo!\nThis is bad:\n   Random number " + actual 
                      + " not equal to " + expected + "!", 
                      expected, actual);
